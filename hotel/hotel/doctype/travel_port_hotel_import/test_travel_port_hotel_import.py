2aea0641b294be03357336732f0fccf004f4a38f
Faisal Zahoor <faisal-zahoor>
2022-02-03 19:27:41 +0500

    document rename

---
+++ hotel/hotel/doctype/travel_port_hotel_import/test_travel_port_hotel_import.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Arslan sheikh and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestTravelPortHotelImport(unittest.TestCase):
+	pass
---
+++ hotel/hotel/doctype/travel_port_hotel_import/travel_port_hotel_import.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Arslan sheikh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Travel Port Hotel Import', {
+	// refresh: function(frm) {
+
+	// }
+});

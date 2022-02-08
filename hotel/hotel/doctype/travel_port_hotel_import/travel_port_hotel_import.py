d38e0169af298f969648ece98b5eb49c84be80ab
Faisal Zahoor <faisal-zahoor>
2022-02-08 10:53:16 +0500

    feat: travelport import hotels

--- hotel/hotel/doctype/travel_port_hotel_import/travel_port_hotel_import.py
+++ hotel/hotel/doctype/travel_port_hotel_import/travel_port_hotel_import.py
@@ -3,6 +3,43 @@
 
 # import frappe
 from frappe.model.document import Document
+import base64
+
 
 class TravelPortHotelImport(Document):
-	pass
+    def fetch_hotels(self):
+        url = "https://americas.universal-api.travelport.com/B2BGateway/connect/uAPI/HotelService"
+
+        auth = base64.b64encode('Universal API/uAPI3989408696-f1474d8f:H+j83kW*}A')
+
+        headers = {
+            'content-type': 'text/xml',
+            'Authorization': f'Basic {auth}'
+        }
+
+        body = """<soapenv: Envelope xmlns: soapenv = "http://schemas.xmlsoap.org/soap/envelope/" >
+					<soapenv: Header/>
+						<soapenv: Body >
+							<hot: HotelSearchAvailabilityReq xmlns: hot = "http://www.travelport.com/schema/hotel_v49_0" TargetBranch = "P3733743" >
+								<com: BillingPointOfSaleInfo xmlns: com = "http://www.travelport.com/schema/common_v49_0" OriginApplication = "UAPI"/>
+								
+								<hot: HotelSearchLocation >
+									<hot: HotelLocation xmlns: com = "http://www.travelport.com/schema/common_v49_0" Location = "IST" LocationType = "City" / >
+								</hot: HotelSearchLocation >
+							
+								<hot: HotelSearchModifiers AvailableHotelsOnly = "false" MaxWait = "50000" NumberOfAdults = "1" >
+									<com: PermittedProviders xmlns: com = "http://www.travelport.com/schema/common_v49_0" >
+										<com: Provider Code = "1G"/>
+									</com: PermittedProviders >
+								</hot: HotelSearchModifiers >
+							
+								<hot: HotelStay >
+									<hot: CheckinDate > 2022-03-15 < /hot:CheckinDate >
+									<hot: CheckoutDate > 2022-03-17 < /hot:CheckoutDate >
+								</hot: HotelStay >
+							</hot: HotelSearchAvailabilityReq >
+						</soapenv: Body >
+					</soapenv: Envelope >"""
+
+        response = requests.post(url, data=body, headers=headers)
+        print(response.content)
